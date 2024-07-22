8cac18dbf971753ff1211273b52cffd0c569887a
RiTmm94 <shabalin.kostya2015@yandex.ru>
2024-07-22 08:02:21 +0500

    Add files via upload

---
+++ modul5.2.py
@@ -0,0 +1,27 @@
+class House:
+    def __init__(self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = number_of_floors
+
+    def __len__(self):
+        return self.number_of_floors
+
+    def __str__(self):
+        return (f'Название: {self.name}, кол-во этажей: {self.number_of_floors}')
+
+h1 = House('ЖК Эльбрус', 10)
+h2 = House('ЖК Акация', 20)
+
+# __str__
+print(h1)
+print(h2)
+
+# __len__
+print(len(h1))
+print(len(h2))
+
+#Вывод на консоль:
+#Название: ЖК Эльбрус, кол-во этажей: 10
+#Название: ЖК Акация, кол-во этажей: 20
+#10
+#20
